8db7f76764d3a5824bc8da3f47df87b9991d3390
madhusudhanreddydhonnapati <msreddy7075@gmail.com>
2024-10-07 22:16:06 +0530

    username added

--- fiwork/src/Navbar.js
+++ fiwork/src/Navbar.js
@@ -1,72 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import JobProvider from './JobProvider';
-import JobNeeded from './JobNeeded';
+import React, { useEffect, useState } from "react";
+import JobProvider from "./JobProvider";
+import JobNeeded from "./JobNeeded";
 import Signin from "./Signin";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  const [username, setusername] = useState("");
 
-function Navbar(){
+  const User = () => {
+    fetch("http://localhost:8080/log/currentUser")
+      .then((res) => res.text())
+      .then((result) => {
+        setusername(result);
+      });
+  };
+  useEffect(() => {
+    User();
+  }, []);
+  console.log("username is: " + username);
 
-  /*
-  const [logdata,setLogdata]=useState("");
-  const logdetails=()=>{
-        fetch("http://localhost:8080/log/currentUser")
-        .then(res=>res.json())
-        .then((result)=>{
-            setLogdata(result);
-        })
-  }
-  useEffect(()=>{
-    logdetails();
-  },[]);
-*/
-
-  return(
+  return (
     <>
-     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Find Work</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav ">
-        <li className="nav-item">
-        <Link className="nav-link"  to="/">Home</Link>
-        </li>
-        
-        <li className="nav-item">
-        <Link className="nav-link"  to="/JobNeeded">JobNeeded</Link>
-        </li>
-        <li className="nav-item">
-         <Link className="nav-link"  to="/JobProvider">JobProvider</Link>
-        </li>
-
-        
-        <ul className="navbar-nav" > 
-          <li className="nav-item ">
-          <Link className="nav-link"  to="/Signin">Sign In</Link>
-          </li>
-        </ul>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Profile
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Find Work
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#"></a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-  
-</nav>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/JobNeeded">
+                  JobNeeded
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/JobProvider">
+                  JobProvider
+                </Link>
+              </li>
+
+              <ul className="navbar-nav">
+                <li className="nav-item ">
+                  <Link className="nav-link" to="/Signin">
+                    Sign In
+                  </Link>
+                </li>
+              </ul>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Profile
+                </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#"></a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <p>User Name : </p>
+                      {username}
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
